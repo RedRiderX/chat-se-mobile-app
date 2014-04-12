4f08933bd93d6581f44184f5412fa61435d1ad33
RedRiderX <contactus@redriderx.com>
2014-04-12 15:16:29 -0400

    trying to test alternate method

--- www/js/index.js
+++ www/js/index.js
@@ -42,7 +42,7 @@ var app = {
     },
 
     launchSite: function() {
-        var ref = window.open('http://google.com', '_self', 'location=yes');
+        var ref = window.open('http://chat.stackexchange.com', '_blank', 'location=yes');
          ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
          ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
          ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
