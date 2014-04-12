adae7a3abd3e13679bfa8ac5d34f2051ab3e3521
RedRiderX <contactus@redriderx.com>
2014-04-12 10:11:29 -0400

    Inital styling for iframe

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        alert("Hello, world");
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
