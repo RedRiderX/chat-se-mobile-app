60bdb55c9872afc7e96705c7e15162e9a3566d17
RedRiderX <contactus@redriderx.com>
2014-04-13 15:15:41 -0400

    first round of style changes

--- www/js/chat.window.js
+++ www/js/chat.window.js
@@ -1,6 +1,11 @@
 $(function() {
 
+	// Add meta tag
 	var viewportTag = '<meta name="viewport" content="width=device-width, initial-scale=1">';
 	$('head').prepend(viewportTag);
+
+	// Move chatroom title
+	$( '#sidebar-content' ).prepend( $( '#sidebar-content .room-title' ) );
+
 	console.log("js injected!");
 });
---
+++ www/js/chat.window.preload.js
@@ -0,0 +1,1 @@
+// Javascript to preload for page.
